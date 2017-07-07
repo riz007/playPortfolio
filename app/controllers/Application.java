16374cfb99c312df6012d0aefe7eaf6ced273623
Riz Rudra <Rizwanul@xyzprinting.com>
2017-07-07 17:29:16 +0700

    New portfolio site on play! framework

---
+++ app/controllers/Application.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+
+
+public class Application extends Controller {
+
+    public static void index() {
+
+        render();
+    }
+
+    public static void btsfare() {
+        render();
+    }
+
+}
+
---
+++ app/controllers/Mails.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by LB670245 on 03-Jul-17.
+ */
+public class Mails {
+}
